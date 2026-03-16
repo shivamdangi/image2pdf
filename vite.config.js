cf7c52403ee10f1d4854ae7ef2c15a842940c585
Shivam Dangi <120727807+shivamdangi@users.noreply.github.com>
2026-03-16 21:48:47 +0530

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react(), sitemapPlugin({
-    hostname: "https://image2pdf-xi.vercel.app/"
-  })],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://image2pdf-xi.vercel.app/"
+    })
+  ]
 });
-
